53a5d5dc494bbb3f5c6065bb291758338bf975cc
ChingisB <c_bogdatov@kbtu.kz>
2022-03-14 18:27:44 +0600

    upd

--- operators/Ножка стула.py
+++ operators/Ножка стула.py
@@ -8,16 +8,11 @@ class ChairLeg:
     def __rmul__(self, m):
         self.length *= m
     
-    def __div__(self, m):
+    def __truediv__(self, m):
         self.length /= m
     
     def __mod__(self, m):
         return self.length % m
-
-
-chl = ChairLeg(15)
-chl * 3
-print(chl.length)
-2 * chl
-print(chl.length)
-print(chl % 7)
+    
+    def __floordiv(self, m):
+        self.n = self.n // m
---
+++ operators/С три козы.py
@@ -0,0 +1,21 @@
+class Dragonfly:
+    def __init__(self, n):
+        self.n = n
+    
+    def __iadd__(self, m):
+        self.n += m
+        self.n = max(1, self.n)
+        self.n = min(30, self.n)
+        return self
+    
+    def __add__(self, other):
+        return Dragonfly(min(30, self.n + other.n))
+    
+    def __truediv__(self, m):
+        return [Dragonfly(self.n // m) for i in range(m)]
+    
+    def __str__(self):
+        return "Dragonfly with a height of {}".format(self.n)
+    
+    def __repr__(self):
+        return 'Dragonfly({})'.format(self.n)
