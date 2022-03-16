489938df1b3560b1850cb96b02363ad75f0fcea0
ChingisB <c_bogdatov@kbtu.kz>
2022-03-16 18:27:17 +0600

    upd

---
+++ operators 2/Правила для Рыцарей.py
@@ -0,0 +1,63 @@
+class Knight:
+    def __init__(self, name, weight, weapons=[]):
+        self.name, self.weight, self.weapons = name, weight, weapons
+    
+    def overturn(self):
+        self.name = self.name[::-1].capitalize()
+        self.weapons = self.weapons[1:] + [self.weapons[0]]
+    
+    def add_weight(self, value):
+        self.weight = max(0, self.weight + value)
+    
+    def add_weapon(self, value):
+        self.weapons.append(value)
+    
+    def get_weapon(self):
+        if self.weapons:
+            return self.weapons[0]
+        return ''
+    
+    def __str__(self):
+        return 'Knight {}, weapon {}, {}'.format(self.name, self.get_weapon(), self.weight)
+    
+    def __eq__(self, other):
+        return (self.name, self.weight, self.weapons) == (other.name, other.weight, other.weapons)
+    
+    def __ne__(self, other):
+        return not self == other
+    
+    def __lt__(self, other):
+        if self == other:
+            return False
+        if self.weight < other.weight:
+            return True
+        elif self.weight == other.weight:
+            if len(self.weapons) < len(other.weapons):
+                return True
+            elif len(self.weapons) == len(other.weapons):
+                if self.name < other.name:
+                    return True
+                return False
+            return False
+        return False
+    
+    def __gt__(self, other):
+        if self == other:
+            return False
+        return other < self
+    
+    def __le__(self, other):
+        if self == other:
+            return True
+        return self < other
+    
+    def __ge__(self, other):
+        return other <= self
+
+
+kn = Knight('Jack', 20, ['sword', 'mace', 'spear'])
+print(kn)
+kn1 = Knight('Jacky', 20, ['sword', 'spear', 'mace'])
+print(kn1)
+print(kn1 <= kn)
+print(kn1 > kn)
