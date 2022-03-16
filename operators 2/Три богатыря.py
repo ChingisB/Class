a15429b0719582c5761ffae39292f56ac68f36ce
ChingisB <c_bogdatov@kbtu.kz>
2022-03-16 18:55:31 +0600

    upd

---
+++ operators 2/Три богатыря.py
@@ -0,0 +1,69 @@
+class EpicHero:
+    def __init__(self, name, wins, weapons=[]):
+        self.name, self.wins, self.weapons = name, wins, weapons
+    
+    def add_win(self):
+        self.wins += 1
+    
+    def add_weapon(self, value):
+        self.weapons.append(value)
+    
+    def get_weapon(self):
+        if self.weapons:
+            return self.weapons[0]
+        return ''
+    
+    def del_weapon(self, value):
+        if value in self.weapons:
+            self.weapons.remove(value)
+    
+    def __str__(self):
+        return 'Epic Hero {}, {}'.format(self.name, self.wins)
+    
+    def __eq__(self, other):
+        return (self.name, self.wins, len(self.weapons)) == (other.name, other.wins, len(other.weapons))
+    
+    def __ne__(self, other):
+        return not self == other
+    
+    def __lt__(self, other):
+        if self == other:
+            return False
+        if self.wins < other.wins:
+            return True
+        elif self.wins == other.wins:
+            if len(self.weapons) < len(other.weapons):
+                return True
+            elif len(self.weapons) == len(other.weapons):
+                if len(self.name) > len(other.name):
+                    return True
+                return False
+            return False
+        return False
+    
+    def __gt__(self, other):
+        if self == other:
+            return False
+        return other < self
+    
+    def __le__(self, other):
+        if self == other:
+            return True
+        return self < other
+    
+    def __ge__(self, other):
+        return other <= self
+    
+    def __repr__(self):
+        return "EpicHero(‘{}’, {}, {})".format(self.name, self.wins, sorted(self.weapons))
+
+eh_1 = EpicHero('Ilya', 1, ['mace', 'bow'])
+eh_2 = EpicHero('Dobrynya', 2, ['knife', 'sword'])
+print(eh_2 < eh_1)
+eh_1.add_win()
+print(eh_1 >= eh_2)
+eh_1.del_weapon('bow')
+print(eh_1 > eh_2)
+eh_3 = EpicHero('Ilya', 2, ['lash'])
+print(eh_1 == eh_3)
+print([eh_1, eh_2, eh_3])
