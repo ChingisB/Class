8c0c0b70b85423929e0ca87953eb22744e791eb2
ChingisB <c_bogdatov@kbtu.kz>
2022-03-16 18:46:47 +0600

    upd

---
+++ operators 2/Вольтов столб.py
@@ -0,0 +1,26 @@
+class VoltaicPile:
+    def __init__(self, values):
+        self.values = values[:]
+        self.i = 0
+    
+    def __len__(self):
+        return len(self.values)
+    
+    def __getitem__(self, key):
+        return self.values[key]
+    
+    def __setitem__(self, key, value):
+        self.values[key] = value
+    
+    def append(self, value):
+        self.values.append(value)
+    
+    def __iter__(self):
+        return iter(self.values)
+    
+    def __next__(self):
+        self.i += 1
+        return self.values[self.i - 1]
+    
+    def __str__(self):
+        return '{} V'.format(''.join(self.values).count('CuclothZn') * 1.1)
