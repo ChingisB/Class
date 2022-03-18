fd38e77a93beda77af096480aa019194592b0ae8
ChingisB <c_bogdatov@kbtu.kz>
2022-03-18 18:11:20 +0600

    upd

---
+++ inherit/Зоопарк частиц.py
@@ -0,0 +1,30 @@
+class Particles:
+    pass
+
+
+class Fermions(Particles):
+    pass
+
+
+class Bosons(Particles):
+    pass
+
+
+class Hadrons(Fermions):
+    pass
+
+
+class Leptons(Fermions):
+    pass
+
+
+class Photons(Bosons):
+    pass
+
+
+class Gluons(Bosons):
+    pass
+
+
+class VectorBosons(Bosons):
+    pass
