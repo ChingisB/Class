58c3bd36e443e0579cc0de830d2f13a362c70c67
ChingisB <c_bogdatov@kbtu.kz>
2022-03-28 18:23:47 +0600

    upd

--- poli/Ромб из букв.py
+++ poli/Ромб из букв.py
@@ -2,37 +2,27 @@ class LettersRhombus:
     def __init__(self, letter, empty=' '):
         self.matrix = []
         self.letter = letter
-        for i in range(1 + 2 * (ord(letter) - ord('A'))):
+        for i in range(ord(letter) - ord('A') + 1):
             t = []
-            for j in range(1 + 2 * (ord(letter) - ord('A'))):
+            for j in range(2 * (ord(letter) - ord('A')) + 1):
                 t.append(empty)
             self.matrix.append(t)
         self.fill()
-    
+
     def fill(self):
-        t = ord(self.letter) - ord('A')
-        letter = 'A'
-        i = 0
-        while t >= 0:
-            self.matrix[i][t] = letter
-            self.matrix[i][len(self.matrix) - t - 1] = letter
-            letter = chr(ord(letter) + 1)
-            t -= 1
-            i += 1
-        t += 2
-        letter = chr(ord(letter) - 2)
-        while t <= 3:
-            if i >= len(self.matrix):
-                break
-            self.matrix[i][t] = letter
-            self.matrix[i][len(self.matrix) - t - 1] = letter
-            letter = chr(ord(letter) - 1)
-            t += 1
-            i += 1
-    
+        symb = 'A'
+        posy, posx = 0, 0
+        while ord(symb) <= ord(self.letter):
+            self.matrix[posx][len(self.matrix[0]) // 2 + posy] = symb
+            self.matrix[posx][len(self.matrix[0]) // 2 - posy] = symb
+            posy, posx = posx + 1, posy + 1
+            symb = chr(ord(symb) + 1)
+        for i in self.matrix[-2:-len(self.matrix) - 1:-1]:
+            self.matrix.append(i)
+
     def rows(self):
         return [''.join(i) for i in self.matrix]
-    
+
     def cols(self):
         ans = []
         for i in range(len(self.matrix)):
@@ -41,7 +31,3 @@ class LettersRhombus:
                 t.append(self.matrix[j][i])
             ans.append(''.join(t))
         return ans
-
-
-lines = LettersRhombus("C")
-print(*lines.cols(), sep="\n")
